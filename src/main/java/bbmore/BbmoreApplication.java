27d199b8d5fc792689e6c6b5fdddeddb3513fe79
soomin0927 <ehfvls0927@naver.com>
2025-03-20 10:39:31 +0900

    test

---
+++ src/main/java/bbmore/BbmoreApplication.java
@@ -0,0 +1,13 @@
+package bbmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbmoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbmoreApplication.class, args);
+    }
+
+}
