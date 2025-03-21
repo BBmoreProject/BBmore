92e0cab1f5e7c9fdf680f6bc91e911b8a74e80bc
BBB924 <qhrud4231@naver.com>
2025-03-21 12:54:42 +0900

    tuitutu

--- src/main/java/com/bbmore/admin/anotice/dto/AdminNoticeDTO.java
+++ src/main/java/com/bbmore/admin/anotice/dto/AdminNoticeDTO.java
@@ -2,7 +2,7 @@ package com.bbmore.admin.anotice.dto;
 
 public class AdminNoticeDTO {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello tuitutu");
     }
 
 }
