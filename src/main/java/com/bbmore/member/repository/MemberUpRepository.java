2cc9fe30e5a649c852f4de470ba91fa2afe48e5b
soomin0927 <ehfvls0927@naver.com>
2025-04-03 16:46:01 +0900

    - 기본 구조

--- src/main/java/com/bbmore/member/repository/MemberUpRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bbmore.member.repository;
-
-import com.bbmore.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberUpRepository extends JpaRepository<Member, Integer> {
-    Member findByUserId(String userId);
-}
